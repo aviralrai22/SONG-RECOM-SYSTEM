ea69fb80503e2ae92c63f1157471dfac0474e73b
unknown <aviralrai22@gmail.com>
2025-09-07 13:17:22 +0530

    modified setup file

--- songs_recommendation_system/setup.py
+++ songs_recommendation_system/setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages,setup
 #project basic information so that if someone want to install the project as ans package can read the information
  
 setup(
-name='machine learning project on house price prediction',
+name='machine learning project on songs_recommendation_system',
 version= '0.0.1',
 author='aviral rai',
 author_email='aviralrai22@gmail.com',
